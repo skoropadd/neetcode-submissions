8c3718dbbc751dee2dfbcfc51fed80b0c157d4c2
Daria Skoropad <101997568+skoropadd@users.noreply.github.com>
2026-06-09 15:19:02 +0200

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        if s == s[::-1]: 
+            return True 
+
+        for i in range(len(s) - 1): 
+            new_s = s[:i] + s[i + 1:]
+            if new_s == new_s[::-1]:
+                return True
+
+        return False
+            
