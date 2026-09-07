390072663ce1bdc893a298b98513e52e5bd09c66
Daria Skoropad <101997568+skoropadd@users.noreply.github.com>
2026-09-07 17:31:28 +0200

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        queue = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    queue.append((r, c))
+
+        dist = 1
+        while queue: 
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in neighbors:
+                    nr, nc = r + dr, c + dc
+                    if (
+                        nr < 0
+                        or nc < 0
+                        or nr == rows
+                        or nc == cols
+                        or grid[nr][nc] != 2147483647
+                    ):
+                        continue
+                    grid[nr][nc] = dist
+                    queue.append((nr, nc))      # ← missing
+            dist += 1
